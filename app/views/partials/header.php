1b742cfd557cb7ff1c36f5a8aafbe2713fb74cfe
denver <denver.delamasa@gmail.com>
2024-10-01 13:23:04 +0800

    nag sstart nako mag front-end

--- app/views/partials/header.php
+++ app/views/partials/header.php
@@ -18,46 +18,29 @@ if (isset($_GET['logout'])) {
   </title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  
+    <style>
+      .navbar{
+        width: 100%;
+        height: 100px;
+        background-color: #F6F2F1;
+        overflow: hidden;
+      }
+      .logo-nav{
+        width: auto;
+        height: auto;
+      }
+      .logo-nav img{
+        width: auto;
+        height: 8%;
+      }
+    </style>
+
 </head>
-
 <body>
-
-  <nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">
-        <?= APP_NAME ?>
-      </a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="<?= SERVER ?>/dashboard">Dashboard</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="<?= SERVER ?>/users">Users</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="<?= SERVER ?>/articles">Articles</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="<?= SERVER ?>/cats">Cats</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="<?= SERVER ?>/profile">Profile</a>
-          </li>
-          <li>
-            <a href="<?= SERVER ?>/login?logout=true" class="nav-link" id="logout">
-            <span class="link">Log Out</span>
-            </a>
-          </li>
-        </ul>
-        <form class="d-flex" role="search">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
+  <div class= "navbar">
+      <div class="logo-nav">
+        <img src="assets/HomePage/resources/Banner.png" alt="">
       </div>
-    </div>
-  </nav>
+  </div>
+</body>
