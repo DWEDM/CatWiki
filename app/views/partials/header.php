e1bc6994752bd6951f25122d53703919b361bd1f
denver <denver.delamasa@gmail.com>
2024-10-07 15:38:35 +0800

    maayos na navbar RAAGHHHH napaka galing ko mugashahahhaha

--- app/views/partials/header.php
+++ app/views/partials/header.php
@@ -11,34 +11,6 @@ if (isset($_GET['logout'])) {
 <html lang="en">
 
 <head>
-  <!-- waowoawoaowoaowoaw HAHSGHAHAHAHHA -->
-
-  <link href="style.css" rel="stylesheet">
-  <script href="script.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-  <!-- Popper.js (necessary for Bootstrap's JavaScript plugins) -->
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
-  <!-- Bootstrap JS -->
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.15/cropper.min.css">
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.15/cropper.min.js"></script>
-  <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-
-  <!-- jQuery and Bootstrap JS -->
-  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
-
-
-
-
 
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -48,12 +20,11 @@ if (isset($_GET['logout'])) {
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   
-    <style>
+</head>
+
+  <style>
       .navbar{
-        width: 100%;
-        height: 200px;
-        background-color: #F6F2F1;
-        overflow: hidden;
+        background: #F6F2F1;
       }
       .logo-nav{
         width: auto;
@@ -64,59 +35,60 @@ if (isset($_GET['logout'])) {
         width: 4%;
         height: auto;
       }
-    </style>
+  </style>
 
-</head>
-<body>
-  <div class= "navbar">
-      <div class="logo-nav">
-        <img src="assets/HomePage/resources/Banner.png" alt="">
-      </div>
-      
-      <div class="navbar-links">
-        <ul class="navbar-nav">
-          <li><a href="">Home</a></li>
-          <li></li> <!--Dat admin/editor lang to-->
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-        </ul>
-      </div>
-  </div>
+
+
+
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a href="<?= SERVER ?>/dashboard">Dashboard</a>
-        </li>
-        <li class="nav-item">
-          <a href="<?= SERVER ?>/users">Users</a>
-        </li>
-        <li class="nav-item">
-          <a href="<?= SERVER ?>/cats">Cats</a>
-        </li>
-        <li class="nav-item">
-          <a href="<?= SERVER ?>/profile">Profile</a>
-        </li>
-        <li class="nav-item">
-          <a href="<?= SERVER ?>/login?logout=true">Log Out</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Articles
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <a class="dropdown-item" href="#">achuchuchu</a>
-            <a class="dropdown-item" href="#">achuchuchu</a>
-            <a class="dropdown-item" href="#">achuchuchu</a>
-          </div>
-        </li>
-      </ul>
+    <div class="container-fluid">
+      <a class="navbar-brand" href="">
+        
+        <img src="assets/Banner.png" alt="" style="height: auto; width: 60px;">
+
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a href="#" class="nav-link">Home</a>
+          </li>
+          <li class="nav-item">
+            <a href="<?= SERVER ?>/dashboard" class="nav-link">Dashboard</a>
+          </li>
+          <li class="nav-item">
+            <a href="<?= SERVER ?>/users"  class="nav-link">Users</a>
+          </li>
+          <li class="nav-item">
+            <a href="<?= SERVER ?>/cats" class="nav-link">Cats</a>
+          </li>
+          <li class="nav-item">
+            <a href="<?= SERVER ?>/profile" class="nav-link">Profile</a>
+          </li>
+
+
+
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">Articles</a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <li><a class="dropdown-item" href="#">achuchu</a></li>
+              <li><a class="dropdown-item" href="#">achuhuc</a></li>
+              <li><a class="dropdown-item" href="#">achuchu</a></li>
+            </ul>
+          </li>
+        </ul>
+
+        <form class="d-flex">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item float-right">
+              <a href="<?= SERVER ?>/login?logout=true" class="nav-link">Log Out</a>
+            </li>
+          </ul>
+        </form>
+      </div>
     </div>
   </nav>
+    
